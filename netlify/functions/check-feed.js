cb6499b52944e67e8f1f14428e8e56fd4a69bf0c
Dan Stoakes <dan.stoakes8@gmail.com>
2025-07-08 11:31:48 +0100

    Update .gitignore

--- netlify/functions/check-feed.js
+++ netlify/functions/check-feed.js
@@ -18,25 +18,26 @@ exports.handler = async () => {
     }
 
     for (const item of newItems.reverse()) {
-        await axios.post(process.env.SLACK_WEBHOOK_URL, {
-            text: `🆕 *${item.title}*\n${item.link}`
-        });
+        // await axios.post(process.env.SLACK_WEBHOOK_URL, {
+        //     text: `🆕 *${item.title}*\n${item.link}`
+        // });
 
-        await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-            content: `🆕 ${item.title}\n${item.link}`
-        });
+        // await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+        //     content: `🆕 ${item.title}\n${item.link}`
+        // });
 
-        await axios.post(process.env.SENDGRID_API_URL, {
-            personalizations: [{ to: [{ email: process.env.SENDGRID_FROM_EMAIL }] }],
-            from: { email: process.env.SENDGRID_TO_EMAIL },
-            subject: `New Shopify Changelog: ${item.title}`,
-            content: [{ type: 'text/plain', value: `${item.link}` }]
-        }, {
-            headers: {
-                Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
-                'Content-Type': 'application/json',
-            }
-        });
+        // await axios.post(process.env.SENDGRID_API_URL, {
+        //     personalizations: [{ to: [{ email: process.env.SENDGRID_FROM_EMAIL }] }],
+        //     from: { email: process.env.SENDGRID_TO_EMAIL },
+        //     subject: `New Shopify Changelog: ${item.title}`,
+        //     content: [{ type: 'text/plain', value: `${item.link}` }]
+        // }, {
+        //     headers: {
+        //         Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`,
+        //         'Content-Type': 'application/json',
+        //     }
+        // });
+        console.log(`🆕 ${item.title}\n${item.link}`);
     }
 
     fs.writeFileSync(SEEN_FILE, JSON.stringify({ lastSeen: newItems[0].pubDate }, null, 2));
